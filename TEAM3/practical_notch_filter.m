02a019dcb4a2becf7b7078f36e36541c51afaef9
Meenakshi2504 <155628289+Meenakshi2504@users.noreply.github.com>
2025-07-03 16:34:28 +0530

    Delete TEAM3 directory

--- TEAM3/practical_notch_filter.m
+++
@@ -1,6 +0,0 @@
-function filtered_signal = practical_notch_filter(signal, fs, notchFreq, Q)
-    wo = notchFreq / (fs / 2); 
-    bw = wo / Q; 
-    [b, a] = iirnotch(wo, bw);
-    filtered_signal = filter(b, a, signal);
-end
